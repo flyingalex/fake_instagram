b9c221a3be3f5ec35e3d7d1301c871d6e05059fe
flyingalex <hulinalex@163.com>
2017-11-30 07:46:20 +0800

    finish chapter 28,实现评论的相关功能

--- Instagram/CommentVC.swift
+++ Instagram/CommentVC.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
-
+import AVOSCloud
 
 var commentuuid = [String]()
 var commentowner = [String]()
 
-class CommentVC: UIViewController {
+class CommentVC: UIViewController, UITextViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var commentTxt: UITextView!
@@ -27,6 +27,15 @@ class CommentVC: UIViewController {
     
     // 储存keyboard大小的变量
     var keyboard = CGRect()
+    
+    // 将从云端获取的数据写进数组
+    var usernameArray = [String]()
+    var avaArray = [AVFile]()
+    var commentArray = [String]()
+    var dateArray = [Date]()
+    
+    // page size
+    var page: Int = 15
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +58,57 @@ class CommentVC: UIViewController {
         self.view.addGestureRecognizer(backSwipe)
         
         aligment()
+        loadComments()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commentArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CommentCell
+        cell.usernameBtn.setTitle(usernameArray[indexPath.row], for: .normal)
+        cell.usernameBtn.sizeToFit()
+        cell.commentLbl.text = commentArray[indexPath.row]
+        avaArray[indexPath.row].getDataInBackground{(data:  Data?, error: Error?) in
+            cell.avaImg.image = UIImage(data: data!)
+        }
+        
+        // 计算时间
+        let from = dateArray[indexPath.row]
+        let now = Date()
+        let components : Set<Calendar.Component> = [.second, .minute, .hour, .day, .weekOfMonth]
+        let difference = Calendar.current.dateComponents(components, from: from, to: now)
+        
+        if difference.second! <= 0 {
+            cell.dateLbl.text = "现在"
+        }
+        
+        if difference.second! > 0 && difference.minute! <= 0 {
+            cell.dateLbl.text = "\(difference.second!)秒."
+        }
+        
+        if difference.minute! > 0 && difference.hour! <= 0 {
+            cell.dateLbl.text = "\(difference.minute!)分."
+        }
+        
+        if difference.hour! > 0 && difference.day! <= 0 {
+            cell.dateLbl.text = "\(difference.hour!)时."
+        }
+        
+        if difference.day! > 0 && difference.weekOfMonth! <= 0 {
+            cell.dateLbl.text = "\(difference.day!)天."
+        }
+        
+        if difference.weekOfMonth! > 0 {
+            cell.dateLbl.text = "\(difference.weekOfMonth!)周."
+        }
+        
+        return cell
     }
 
     @objc func keyboardWillShow(_ notification: Notification) {
@@ -63,6 +123,7 @@ class CommentVC: UIViewController {
             
         })
     }
+    
     
     @objc func keyboardWillHide(_ notification: Notification) {
         UIView.animate(withDuration: 0.4, animations: {() -> Void in
@@ -86,6 +147,39 @@ class CommentVC: UIViewController {
     // 当控制器从屏幕消失时
     override func viewWillDisappear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        // 如果没有输入信息就禁止按钮
+        let spacing = CharacterSet.whitespacesAndNewlines
+        if !textView.text.trimmingCharacters(in: spacing).isEmpty {
+            sendBtn.isEnabled = true
+        } else {
+            sendBtn.isEnabled = false
+        }
+        
+        if textView.contentSize.height > textView.frame.height &&
+            textView.frame.height < 130 {
+            let difference = textView.contentSize.height -
+                textView.frame.height
+            textView.frame.origin.y = textView.frame.origin.y - difference
+            textView.frame.size.height = textView.contentSize.height
+            
+            // 上移tableView
+            if textView.contentSize.height + keyboard.height + commentY >= tableView.frame.height {
+                tableView.frame.size.height =
+                tableView.frame.size.height - difference
+            }
+        } else if textView.contentSize.height < textView.frame.height {
+            let difference = textView.frame.height - textView.contentSize.height
+            textView.frame.origin.y = textView.frame.origin.y + difference
+            textView.frame.size.height = textView.contentSize.height
+            
+            // 上移tableView
+            if textView.contentSize.height + keyboard.height + commentY > tableView.frame.height {
+                tableView.frame.size.height = tableView.frame.size.height + difference
+            }
+        }
     }
     
     @objc func back(_ sender: UIBarButtonItem) {
@@ -122,5 +216,94 @@ class CommentVC: UIViewController {
         tableViewHeight = tableView.frame.height
         commentHeight = commentTxt.frame.height
         commentY = commentTxt.frame.origin.y
+        
+        commentTxt.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func loadComments() {
+        // STEP 1. 合计出所有的评论的数量
+        let countQuery = AVQuery(className: "Comments")
+        countQuery.whereKey("to", equalTo: commentuuid.last!)
+        countQuery.countObjectsInBackground({ (count: Int, error: Error?) in
+            if self.page < count {
+                self.refresher.addTarget(self, action: #selector(self.loadMore), for: .valueChanged)
+                self.tableView.addSubview(self.refresher)
+            }
+            
+            // STEP 2. 获取最新的self.page数量的评论
+            let query = AVQuery(className: "Comments")
+            query.whereKey("to", equalTo: commentuuid.last!)
+            query.skip = count - self.page
+            query.addAscendingOrder("createdAt")
+            query.findObjectsInBackground({ (objects:[Any]?, error:Error?) in
+                if error == nil {
+                    // 清空数组
+                    self.usernameArray.removeAll(keepingCapacity: false)
+                    self.commentArray.removeAll(keepingCapacity: false)
+                    self.avaArray.removeAll(keepingCapacity: false)
+                    self.dateArray.removeAll(keepingCapacity: false)
+                    
+                    for object in objects! {
+                        self.usernameArray.append((object as AnyObject).object(forKey: "username") as! String)
+                        self.avaArray.append((object as AnyObject).object(forKey: "ava") as! AVFile)
+                        self.commentArray.append((object as AnyObject).object(forKey: "comment") as! String)
+                        self.dateArray.append((object as AnyObject).createdAt as! Date)
+                        
+                        self.tableView.reloadData()
+                        
+                        self.tableView.scrollToRow(at: IndexPath(row: self.commentArray.count - 1, section: 0)  , at: .bottom, animated: false)
+                    }
+                }else {
+                    print(error?.localizedDescription)
+                }
+            })
+        })
+    }
+    
+    @objc func loadMore() {
+        // STEP 1. 合计出所有的评论的数量
+        let countQuery = AVQuery(className: "Comments")
+        countQuery.whereKey("to", equalTo: commentuuid.last!)
+        countQuery.countObjectsInBackground({ (count:Int, error:Error?) in
+            // 让refresh停止刷新动画
+            self.refresher.endRefreshing()
+            
+            if self.page >= count {
+                self.refresher.removeFromSuperview()
+            }
+            
+            // STEP 2. 载入更多的评论
+            if self.page < count {
+                self.page = self.page + 15
+                
+                // 从云端查询page个记录
+                let query = AVQuery(className: "Comments")
+                query.whereKey("to", equalTo: commentuuid.last!)
+                query.skip = count - self.page
+                query.addAscendingOrder("createdAt")
+                query.findObjectsInBackground({ (objects:[Any]?, error:Error?) in
+                    if error == nil {
+                        // 清空数组
+                        self.usernameArray.removeAll(keepingCapacity: false)
+                        self.commentArray.removeAll(keepingCapacity: false)
+                        self.avaArray.removeAll(keepingCapacity: false)
+                        self.dateArray.removeAll(keepingCapacity: false)
+                        
+                        for object in objects! {
+                            self.usernameArray.append((object as AnyObject).object(forKey: "username") as! String)
+                            self.avaArray.append((object as AnyObject).object(forKey: "ava") as! AVFile)
+                            self.commentArray.append((object as AnyObject).object(forKey: "comment") as! String)
+                            self.dateArray.append((object as AnyObject).createdAt as! Date)
+                        }
+                        self.tableView.reloadData()
+                    }else {
+                        print(error?.localizedDescription)
+                    }
+                })
+            }
+        })
     }
 }
+
